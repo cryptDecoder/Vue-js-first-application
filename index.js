62c98aa0108236eee318dd798b534fe6fe990924
darkhost <cryptDecoder@gmail.com>
2021-11-03 23:10:38 +0530

    Vue is not working

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const buttonEL = document.querySelector('button')
-const inputEL = document.querySelector('input')
-const listEL = document.querySelector('ul')
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
+}).mount('#app');
 
-function addMyGoal() {
-    console.log("adding my goals to list")
-    const enteredGoal = inputEL.value
-    console.log(enteredGoal)
-    const listItemEL = document.createElement('li')
-    listItemEL.textContent = enteredGoal
-    listEL.appendChild(listItemEL)
-    inputEL.value = ''
+// const buttonEL = document.querySelector('button')
+// const inputEL = document.querySelector('input')
+// const listEL = document.querySelector('ul')
+
+// function addMyGoal() {
+//     console.log("adding my goals to list")
+//     const enteredGoal = inputEL.value
+//     console.log(enteredGoal)
+//     const listItemEL = document.createElement('li')
+//     listItemEL.textContent = enteredGoal
+//     listEL.appendChild(listItemEL)
+//     inputEL.value = ''
     
-}
+// }
 
-buttonEL.addEventListener('click', addMyGoal);
+// buttonEL.addEventListener('click', addMyGoal);
